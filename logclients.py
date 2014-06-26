77f01b91798d72fa84527fb5f1e808427efeb57b
Eskild Schroll-Fleischer <esf@studerende.dk>
2014-06-26 12:12:06 +0000

    Added logclients.py. A small script that keeps track of devices it has seen.

---
+++ logclients.py
@@ -0,0 +1,29 @@
+from kismetclient import Client as KismetClient
+from kismetclient import handlers
+ 
+# Connect to the Kismet server.
+address = ('127.0.0.1', 2501)
+k = KismetClient(address)
+ 
+# Sets are nice because they only keep unique data.
+try:
+    from sets import Set
+    clients = Set()
+except Exception: # In python3 you don't have to explicitly import Sets
+    clients = set()
+ 
+def handle_client(client, mac):
+    global clients
+    clients.add(mac)
+ 
+k.register_handler('CLIENT', handle_client)                       
+ 
+try:
+    print('Logging wireless network clients.')
+    while True:
+        k.listen()
+except KeyboardInterrupt:
+    print('Clients:')
+    for i, client in enumerate(clients):
+        print('%d. MAC: %s' % (i, client))
+    print('%d unique clients have been seen.' % len(clients))
